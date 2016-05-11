bf00146f3955b0bbfb43a11b8becd5a20b2f061c
germaniuz <germanius@list.ru>
2016-05-11 14:27:46 +0300

    Add back2top

---
+++ js/to_top.js
@@ -0,0 +1,21 @@
+jQuery(document).ready(function () {
+  jQuery('a.back2top').bind("click", function (e) {
+    var anchor = jQuery(this);
+    jQuery('html, body').stop().animate({
+      scrollTop: jQuery(anchor.attr('href')).offset().top
+    }, 1000);
+    e.preventDefault();
+  });
+  var topButton = jQuery('.scrollFadeIn');
+  topButton.hide();
+  jQuery(function () {
+    jQuery(window).scroll(function () {
+      if (jQuery(this).scrollTop() > 100) {
+        topButton.fadeIn();
+      } else {
+        topButton.fadeOut();
+      }
+    });
+  });
+  return false;
+});
