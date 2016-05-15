f6ab0c6ef5aaeaa982c5982a90b86a9cea813c9e
germaniuz <germanius@list.ru>
2016-05-15 12:59:01 +0300

    Add php mailer script

---
+++ mailer.php
@@ -0,0 +1,32 @@
+﻿<?php
+
+if (isset($_POST['phone']) || isset($_POST['email'])){
+
+	$message ='<h2>Заявка с сайта «Refit Auto»</h2>';
+	
+	if(isset($_POST['name'])){
+		$message .= '<p>Имя: <b>'.$_POST['name'].'</b></p>';
+	}
+	
+	if(isset($_POST['phone'])){
+		$message .= '<p>Телефон: <b>'.$_POST['phone'].'</b></p>';
+	}	
+
+	if(isset($_POST['email'])){
+		$message .= '<p>E-mail: <b>'.$_POST['email'].'</b></p>';
+	}
+	
+	include "class.phpmailer.php";
+
+	$mail = new PHPMailer();
+	$mail->From = $_POST['email'] ? $_POST['email'] : "noreply@".$_SERVER['HTTP_HOST'];
+	$mail->FromName = $_REQUEST['name'] ? $_REQUEST['name'] : '-не указано-';
+	$mail->AddAddress('germanius@list.ru');
+	$mail->IsHTML(true);
+	$mail->Subject = "Test message from progressia landing page";
+	$mail->Body = $message;	
+	
+	if (!$mail->Send()) die ('Mailer Error: '.$mail->ErrorInfo);
+	echo  "Ваша заявка отправлена.";
+	
+} ?>
